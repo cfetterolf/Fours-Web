8428f3024106d0f2c7d50b038d37cf3b11263854
Chris <Chris@192.168.1.6>
2017-08-05 10:41:10 -0400

    Updated OG protocol for social media

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
 
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapse-navbar" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
@@ -70,7 +70,7 @@
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <div class="collapse navbar-collapse" id="collapse-navbar">
       <ul class="nav navbar-nav" style="margin-left: 20px;">
         <li class="active"><a href="#home">Home <span class="sr-only">(current)</span></a></li>
         <li><a href="#about">About</a></li>
